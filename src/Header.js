8699f43e7d1c466370487624ce296e4487e5244f
Patricia Ortuño <patricia.ortuno28@gmail.com>
2021-12-16 09:25:13 +0100

    Ultimas modificaciones header.

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,5 @@ export default function Header (props) {
         <h2 id="h2_header">
             Turno de {props.turno}
         </h2>
-        <span id="span_header">
-            <input type="checkbox" name="" id="switch"></input>
-            <label for="switch"></label>
-        </span>
     </div>
 }
