6f83812423592f182bc1cb0b79c232e47fdffd76
Patricia Ortuño <patricia.ortuno28@gmail.com>
2021-12-18 10:37:51 +0100

    Terminada internacionalización home y tictactoe.

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Home.css";
 import { LangContext } from './LangContext';
 
-class Home extends Component {
-  static contextType = LangContext;
-  render () {
-    return (
-      <LangContext.Consumer>
-        {lang =>
-          <div id="container_home">
-            <h1 id="h1_home">{lang.home_welcome}</h1>
-            <h2 id="h2_home">{lang.home_subtitle}</h2>
-            <p>{lang.home_text}</p>
-            <div class="footer">
-              <div class="footer_contents">
-                <b>IWEB 2021</b>
-                &emsp;
-                Daniel Sanz Sobrino
-                &emsp;
-                Patricia Ortuño Otero
-              </div>
+export default function Home () {
+  return (
+    <LangContext.Consumer>
+      {lang =>
+        <div id="container_home">
+          <h1 id="h1_home">{lang.home_welcome}</h1>
+          <h2 id="h2_home">{lang.home_subtitle}</h2>
+          <p>{lang.home_text}</p>
+          <div class="footer">
+            <div class="footer_contents">
+              <b>IWEB 2021</b>
+              &emsp;
+              Daniel Sanz Sobrino
+              &emsp;
+              Patricia Ortuño Otero
             </div>
-        </div>
-        }
-      </LangContext.Consumer>
-    )
-  }
+          </div>
+      </div>
+      }
+    </LangContext.Consumer>
+  )
 }
-
-export default Home;
