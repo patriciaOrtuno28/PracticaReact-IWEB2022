da71e57cf6099b43ac79358973293ce7b1d6cff1
Patricia Ortuño <patricia.ortuno28@gmail.com>
2021-12-18 12:53:13 +0100

    Terminada implementación del juego quiz con internacionalización.

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,15 @@
 import "./Quiz.css";
 import {Button} from 'react-bootstrap';
 import  {useState} from 'react';
+import Shortcuts from "./Shortcuts";
+import { LangContext } from './LangContext';
 
 export default function Game (props) {
     let nullArray = new Array(props.nQuizzes).fill(false);
     const [answers, setAnswers] = useState(nullArray);
     var localScore = 0;
 
-    const before = () => {
+    const checkValid = () => {
         let inputValue = document.getElementById("quiz_answer").value;
         if (inputValue.length>0){
             if (inputValue.toLowerCase() === props.quiz.answer.toLowerCase()){
@@ -19,35 +21,20 @@ export default function Game (props) {
             }
         }
         document.getElementById("quiz_answer").value = "";
+    }
+
+    const before = () => {
+        checkValid();
         props.setCurrentQuiz(props.currentQuiz  - 1);
     }
 
     const next = () => {
-        let inputValue = document.getElementById("quiz_answer").value;
-        if (inputValue.length>0){
-            if (inputValue.toLowerCase() === props.quiz.answer.toLowerCase()){
-                answers[props.currentQuiz] = true;
-                setAnswers(answers);
-            } else {
-                answers[props.currentQuiz] = false;
-                setAnswers(answers);
-            }
-        }
-        document.getElementById("quiz_answer").value = "";
+        checkValid();
         props.setCurrentQuiz(props.currentQuiz  + 1);
     }
 
     const submit = () => {
-        let inputValue = document.getElementById("quiz_answer").value;
-        if (inputValue.length>0){
-            if (inputValue.toLowerCase() === props.quiz.answer.toLowerCase()){
-                answers[props.currentQuiz] = true;
-                setAnswers(answers);
-            } else {
-                answers[props.currentQuiz] = false;
-                setAnswers(answers);
-            }
-        }
+        checkValid();
         answers.forEach(function(element) {
             if (element) {
                 localScore += 1;
@@ -65,36 +52,52 @@ export default function Game (props) {
     }
 
     if (!props.finished) {
-        return <div id="container_gameGlobal">
-            <div id="container_game">
-                <div id="container_img">
-                    <img src={props.quiz.attachment.url} id="img_quiz" onError={event => {
-                        event.target.src = 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg'
-                        event.onerror = null
-                    }}/>
-                    <div id="container_img_text">
-                        <img src={props.quiz.author.photo.url} id="img_author"/>
-                        <p id="text_author">{props.quiz.author.username}</p>
+        return (<LangContext.Consumer>
+            {lang =>
+                <div id="container_gameGlobal">
+                <div id="box1">
+                    <Shortcuts quizzes={props.quizzes} setCurrentQuiz={props.setCurrentQuiz} checkValid={checkValid}/>
+                </div>
+                <div id="container_game">
+                    <div id="container_questions_game">
+                        <div id="container_img">
+                            <img src={props.quiz.attachment ? props.quiz.attachment.url : 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg'} id="img_quiz" onError={event => {
+                                event.target.src = 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg'
+                                event.onerror = null
+                            }}/>
+                            <div id="container_img_text">
+                                <img src={props.quiz.author.photo ? props.quiz.author.photo.url : 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg'} id="img_author" onError={event => {
+                                event.target.src = 'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg'
+                                event.onerror = null
+                            }}/>
+                                <p id="text_author">{props.quiz.author.username ? props.quiz.author.username : 'Nombre no disponible.'}</p>
+                            </div>
+                        </div>
+                        <div id="container_questions">
+                            <h2 id="h2_nQuestion">{lang.game_question}{props.quiz.id}</h2>
+                            <h3 id="h3_question">{props.quiz.question}</h3>
+                            <label for="quiz_answer">{lang.game_insertAnswer}</label>
+                            <input type="text" id="quiz_answer" name="quiz_answer"/>
+                        </div>
+                    </div>
+                    <div id="container_btns">
+                        <Button id="btn_quiz" onClick={before} disabled={props.currentQuiz==0}>{lang.btn_prev}</Button>
+                        <Button id="btn_quiz" onClick={next} disabled={props.currentQuiz==props.nQuizzes-1}>{lang.btn_next}</Button>
+                        <Button id="btn_quiz" onClick={submit}>{lang.btn_submit}</Button>
                     </div>
                 </div>
-                <div id="container_questions">
-                    <h2 id="h2_nQuestion">Question {props.quiz.id}</h2>
-                    <h3 id="h3_question">{props.quiz.question}</h3>
-                    <label for="quiz_answer">Introducir respuesta: </label>
-                    <input type="text" id="quiz_answer" name="quiz_answer"/>
-                </div>
             </div>
-            <div id="container_btns">
-                <Button id="btn_quiz" onClick={before} disabled={props.currentQuiz==0}>Anterior</Button>
-                <Button id="btn_quiz" onClick={next} disabled={props.currentQuiz==props.nQuizzes-1}>Siguiente</Button>
-                <Button id="btn_quiz" onClick={submit}>Submit</Button>
-            </div>
-        </div>
+            }
+        </LangContext.Consumer>);
     } else {
-        return <div id="container_playAgain">
-            <h2>Puntuación: {props.score}</h2>
-            <h3>{(parseInt(props.score)/parseInt(props.nQuizzes))*100}% acertadas</h3>
-            <Button id="btn_quiz" onClick={playAgain}>Volver a jugar</Button>
-        </div>
+        return (<LangContext.Consumer>
+            {lang =>
+                <div id="container_playAgain">
+                    <h2>{lang.game_score}{props.score}</h2>
+                    <h3>{(parseInt(props.score)/parseInt(props.nQuizzes))*100}% {lang.game_correct}</h3>
+                    <Button id="btn_quiz" onClick={playAgain}>{lang.game_playAgain}</Button>
+                </div>
+            }
+        </LangContext.Consumer>);
     }
 }
--- src/Quiz.js
+++ src/Quiz.js
@@ -2,50 +2,40 @@ import React from "react";
 import Game from "./Game";
 import  {useState} from 'react';
 import  {useEffect} from 'react';
-import Shortcuts from './Shortcuts';
 import "./Quiz.css";
 import axios from "axios";
-import {quizzes} from './assets/mock-data'
+import { LangContext } from './LangContext';
 
-
-
-
-export default function Quiz() {
-  
-  const [articulos, setArticulos] = useState([]);
-  const temp = [];
-  useEffect(() => {
-    axios.get('https://core.dit.upm.es/api/quizzes/random10wa?token=0a72635966eb3864d6fe')
-      .then((response) => {
-        return response.data
-      })
-      .then((response) => response.json())
-      .then((articulos) => {
-        articulos.forEach(function(element){
-          temp.append(element);
-        });
-      })
-  });
-
-  setArticulos(temp);
-  console.log(articulos);
- 
-  
+export default function Quiz() {  
   // Inicialización de parámetros como Hooks
   const [score, setScore] = useState(0);
   const [finished, setFinished] = useState(false);
   const [currentQuiz, setCurrentQuiz] = useState(0);
-  const [quizzesArray, setQuizzes] = useState(quizzes);
+  const [quizzesArray, setQuizzes] = useState([]);
+
+  // Usamos axios por su bajo peso, rapidez en las peticiones y soporte de plataformas  
+  useEffect(() => {
+    const consultaAPI = async () => {
+      await axios({
+        method: 'GET',
+        url: 'https://core.dit.upm.es/api/quizzes/random10wa?token=0a72635966eb3864d6fe'
+      }).then(res => {    // Es una promesa -> .then
+        setQuizzes(res.data);
+      }).catch(err => console.log(err))
+    }
+    consultaAPI();
+  }, []);
 
   // Visualización
   return(
-    <div id="container_quiz">
-      <div id="box1">
-        <Shortcuts quizzes={quizzesArray} setCurrentQuiz={setCurrentQuiz}/>
-      </div>
-      <div id="box2">
-        <Game quiz={quizzesArray[currentQuiz]} currentQuiz={currentQuiz} setCurrentQuiz={setCurrentQuiz} nQuizzes={quizzesArray.length} setScore={setScore} score={score} setFinished={setFinished} finished={finished}/>
-      </div>
-    </div>
+    <LangContext.Consumer>
+        {lang =>
+            <div id="container_quiz">
+            {
+              quizzesArray.length >= 1 ? <Game quizzes={quizzesArray} quiz={quizzesArray[currentQuiz]} currentQuiz={currentQuiz} setCurrentQuiz={setCurrentQuiz} nQuizzes={quizzesArray.length} setScore={setScore} score={score} setFinished={setFinished} finished={finished}/> : <h2>No hay quizzes disponibles.</h2>
+            }
+          </div>
+        }
+    </LangContext.Consumer>
   );
 }
